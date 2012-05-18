15d9fb2f0ce61f7dfba5fd7f655ce3dc0b10b7b7
Giovanni <giofouche@fullsail.edu>
2012-05-17 21:38:48 -0400

    added vacation function

--- Fouche_Giovanni_Assignment3.js
+++ Fouche_Giovanni_Assignment3.js
@@ -1,1 +1,26 @@
 // Giovanni Fouche
+
+
+var newYork = {
+    travelers: ["Bethany", " Anthony", " Kristina "]
+};
+
+var springBreak = {
+    type: "New York",
+    days: 3,
+    myVacation: function (trip) {
+        console.log("We want to take a " + this.type + " " + trip + ". Let me see how many days I can take off work.");
+        if (this.days >= 5) {
+            console.log("Looks like we can take a week long" + trip + "!")
+        } else {
+            if (this.days < 5 && this.days >= 1) {
+                console.log("Looks Like we can only spend a few days.")
+            } else {
+                console.log("I can't get time off looks like we won't be going.")
+            }
+        }
+    }
+
+};
+
+
