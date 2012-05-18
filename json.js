ef0c929bb424721f2d53382219a71968e8cc5107
Giovanni <giofouche@fullsail.edu>
2012-05-17 21:32:22 -0400

    first commit

---
+++ json.js
@@ -0,0 +1,43 @@
+var json = {
+	"guests": {
+		"Giovanni": {
+			"name": "Giovanni",
+			"age": 20
+		},
+		"Bethany": {
+			"name": "Bethany",
+			"age": 18
+		},
+		"Anthony": {
+			"name": "Anthony",
+			"age": 21
+		},
+		"Kristina": {
+			"name": "Kristina",
+			"age": 18
+		}
+		
+	}
+};
+
+
+
+var json2 = {
+		"lodging": [
+		{
+			"hotel": "Millennium Broadway Hotel",
+			"pricePerNight": "258",
+			"breakfast": false
+		},
+		{
+			"hotel": "Verve Hotel",
+			"pricePerNight": "149",
+			"breakfast": true
+		},
+		{
+			"hotel": "Four Points",
+			"pricePerNight": "219",
+			"breakfast": false
+		}
+	]
+};
